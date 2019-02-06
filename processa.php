5329a315865c5407d4d9532ac9d4887e82e3e63e
goncalvesmatheus <ms.goncalves98@gmail.com>
2019-02-06 20:35:17 -0200

    update form

--- processa.php
+++ processa.php
@@ -1,19 +1,32 @@
 <?php
-    
-    $remetente = 'humberto@hdcinfraestrutura.com.br';
-    $destinatario = 'humberto@hdcinfraestrutura.com.br';
+
+    // Dados do formulário
     $nome = $_POST['nome'];
     $email = $_POST['email'];
     $telefone = $_POST['telefone'];
-    $mensagem = $_POST['mensagem'];
+    $mensagem_site = $_POST['mensagem_site'];
+
+    // Variáveis de envio
+    $remetente = "humberto@hdcinfraestrutura.com.br";
+    $destino = "humberto@hdcinfraestrutura.com.br";
+    $assunto = "Contato através do site";
+
+    // Corpo da mensagem
+    $mensagem = "Olá, você tem uma nova mensagem!" . "</br>";
+    $mensagem .= "Nome do contato: " . $nome . "</br>";
+    $mensagem .= "E-mail: " . $email . "</br>";
+    $mensagem .= "Telefone de contato: " . $telefone . "</br>";
+    $mensagem .= "Mensagem: " .  "</br>";
+    $mensagem .= $mensagem_site;
+
+
 
     $alerta = 'Formulário enviado, obrigado pelo contato!';
 
 
-    mail($destinatario, $nome, $email, $telefone, $mensagem);
+    return mail($destino, $assunto, $nome, $email, $telefone, $mensagem);
 
     echo $alerta;
 
-    
     
 ?>
